54a3b6ee9f090adbd32b059a5c3059f15960cb67
Sidney Klass <sidney.klass@leanix.net>
2021-09-08 09:38:45 +0200

    add Day 3

---
+++ Day 3 - Hex Color/script.js
@@ -0,0 +1,10 @@
+const count = document.querySelector (".count")
+const buttons = document.querySelector (".buttons")
+
+
+buttons.addEventListener("click", (event) => {
+    let color = Math.random().toString(16).substring(2, 8)
+    count.innerHTML = `#${color}`;
+    document.body.style.backgroundColor = `#${color}`
+})
+console.log(Math.random().toString(16).substring(2, 8))
