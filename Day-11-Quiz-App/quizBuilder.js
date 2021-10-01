6429de957c82bada3fcdb2919f991b26217aff42
Sidney Klass <sidney.klass@leanix.net>
2021-10-01 16:22:15 +0200

    start setting up QuizBuilder

--- Day-11-Quiz-App/quizBuilder.js
+++ Day-11-Quiz-App/quizBuilder.js
@@ -7,9 +7,21 @@ class QuizBuilder {
     _quizBuilderHTMLBody() {
         return `
         <h1>Quiz Builder</h1>
-        <input type="text" class="input-" placeholder="Enter a word..." />
-        <hr>
-        <div class="inputQuestions">
+        <form> 
+            <div class="form-group">
+                <label for="inputName">Enter a Quiz Name</label>
+                <input type="Quizname" class="form-control form-control-lg" id="inputName" placeholder="Enter a name...">
+                <small>Ja</small>
+            </div>
+            <hr>
+            <h2>Questions</h2>
+            <hr>
+            <div class="inputQuestions">\
+                <input type="text" class="input-" placeholder="Type in a question..." />
+                <input type="text" class="input-" placeholder="Type in the choices..." />
+                <input type="text" class="input-" placeholder="Type in the correct answer..." />
+            </div>
+        </form>
         <hr>
         <button class="btn" id="quitQuizButtonEnd" style="text-align: center; background-color:#3399ff;" >Quit</button>
     `
@@ -20,11 +32,11 @@ class QuizBuilder {
     }
 
     start() {
-        
-        this.containerElement.innerHTML = this._quizBuilderHTMLBody();
-        const button = document.getElementById('quitQuizButton');
-        button.onclick = () => this.quit();
-        const startEvent = new Event("SidneyQuiz:start");
-        this.containerElement.dispatchEvent(startEvent);
+        const containerElement = document.getElementById("createQuiz")
+        containerElement.innerHTML = this._quizBuilderHTMLBody();
+        // const button = document.getElementById('quitQuizButton');
+        // button.onclick = () => this.quit();
+        // const startEvent = new Event("SidneyQuiz:start");
+        // this.containerElement.dispatchEvent(startEvent);
     }
 }
