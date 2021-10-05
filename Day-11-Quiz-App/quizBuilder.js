5849e3aeec3f0055e83f8a32932fa5e882f2cd97
Sidney Klass <sidney.klass@leanix.net>
2021-10-05 16:24:45 +0200

    Extract method

--- Day-11-Quiz-App/quizBuilder.js
+++ Day-11-Quiz-App/quizBuilder.js
@@ -52,41 +52,7 @@ class QuizBuilder {
     }
 
     save() {
-        // Step 1: We select all the input elements that contain the info that we need to construct our question objects.
-        // querySelectorAll returns the elements in the order as they are in the HTML.
-        const formElements = document.querySelectorAll('.questionText, .questionChoices, .questionAnswer');
-        // Step 2: we create an empty object where we can write the properties of the questions into, until one question is complete, at which point we empty it again.
-        // This is just a "helper object"
-        let tempQuestion = {};
-        
-        // Step 3: document.querySelectorAll returns a NodeList object, which does not support any of the array function we are used to.
-        // We use Array.from() to convert it into an Array.
-        const formElementsArray = Array.from(formElements);
-        
-        // Step 4: Loop over each element, get their value and put it into the matching Question property based on their CSS class.
-        const questions = formElementsArray.reduce((questions, element) => {
-            // we can rely on the order of the three inputs to know
-            // when one question ends and a new one starts
-            if (element.classList.contains('questionText')) {
-            // since this element has the .questionText CSS class we know that its value should be the text of the question.
-                tempQuestion.text = element.value;
-            } else if (element.classList.contains('questionChoices')) {
-            // since this element has the .questionChoices CSS class we know that its value should be the choices of the question.
-                tempQuestion.choices = element.value
-                .split(',') // Turn it into an array, where comma is the delimeter.
-                .map((choice) => choice.trim()); // trim cuts of spaces to the left and right of a choice
-            } else if (element.classList.contains('questionAnswer')) {
-            // since this element has the .questionAnswer CSS class we know that its value should be the answer of the question.
-                tempQuestion.answer = element.value;
-            // At this point our tempQuestion object contains a complete question and we can push it into our questions array.
-                questions.push(tempQuestion);
-        
-            // If there are input elements remaining in the formElementsArray then the element in
-            // the next iteration will be the .questionText input of the next question.
-                tempQuestion = {};
-            }
-            return questions;
-        }, []);
+        const questions = this._getQuestionsArrayFromInputValues();
         const quizTitle = document.getElementById('quizTitle').value;
         const quiz = {
             title: quizTitle,
@@ -140,5 +106,43 @@ class QuizBuilder {
         questionText.className = "inputStyles questionText";
         choices.className = "inputStyles questionChoices";
         answer.className = "inputStyles questionAnswer";
+    }
+
+    _getQuestionsArrayFromInputValues() {
+        // Step 1: We select all the input elements that contain the info that we need to construct our question objects.
+        // querySelectorAll returns the elements in the order as they are in the HTML.
+        const formElements = document.querySelectorAll('.questionText, .questionChoices, .questionAnswer');
+        // Step 2: we create an empty object where we can write the properties of the questions into, until one question is complete, at which point we empty it again.
+        // This is just a "helper object"
+        let tempQuestion = {};
+        
+        // Step 3: document.querySelectorAll returns a NodeList object, which does not support any of the array function we are used to.
+        // We use Array.from() to convert it into an Array.
+        const formElementsArray = Array.from(formElements);
+        
+        // Step 4: Loop over each element, get their value and put it into the matching Question property based on their CSS class.
+        return formElementsArray.reduce((questions, element) => {
+            // we can rely on the order of the three inputs to know
+            // when one question ends and a new one starts
+            if (element.classList.contains('questionText')) {
+            // since this element has the .questionText CSS class we know that its value should be the text of the question.
+                tempQuestion.text = element.value;
+            } else if (element.classList.contains('questionChoices')) {
+            // since this element has the .questionChoices CSS class we know that its value should be the choices of the question.
+                tempQuestion.choices = element.value
+                .split(',') // Turn it into an array, where comma is the delimeter.
+                .map((choice) => choice.trim()); // trim cuts of spaces to the left and right of a choice
+            } else if (element.classList.contains('questionAnswer')) {
+            // since this element has the .questionAnswer CSS class we know that its value should be the answer of the question.
+                tempQuestion.answer = element.value;
+            // At this point our tempQuestion object contains a complete question and we can push it into our questions array.
+                questions.push(tempQuestion);
+        
+            // If there are input elements remaining in the formElementsArray then the element in
+            // the next iteration will be the .questionText input of the next question.
+                tempQuestion = {};
+            }
+            return questions;
+        }, []);
     }
 }
