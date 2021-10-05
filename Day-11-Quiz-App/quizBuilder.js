65f2ebee4c045be491c0451216ba3b873eea664b
Sidney Klass <sidney.klass@leanix.net>
2021-10-05 13:21:49 +0200

    Implement input rendering and quit button for quizbuilder

--- Day-11-Quiz-App/quizBuilder.js
+++ Day-11-Quiz-App/quizBuilder.js
@@ -1,42 +1,91 @@
 class QuizBuilder {
-    constructor() {
-        const containerElement = document.getElementById("createQuiz")
-
+    constructor(entryElementId) {
+        this.containerElement = document.getElementById(entryElementId)
     }
 
     _quizBuilderHTMLBody() {
         return `
         <h1>Quiz Builder</h1>
-        <form> 
-            <div class="form-group">
-                <label for="inputName">Enter a Quiz Name</label>
-                <input type="Quizname" class="form-control form-control-lg" id="inputName" placeholder="Enter a name...">
-                <small>Ja</small>
+        <form>
+            <div>
+                <h2 id="quizNameHeader"> Enter a Quiz Name:</h2>
             </div>
             <hr>
             <h2>Questions</h2>
             <hr>
-            <div class="inputQuestions">\
-                <input type="text" class="input-" placeholder="Type in a question..." />
-                <input type="text" class="input-" placeholder="Type in the choices..." />
-                <input type="text" class="input-" placeholder="Type in the correct answer..." />
+            <div id="questionsContainer">
             </div>
         </form>
         <hr>
-        <button class="btn" id="quitQuizButtonEnd" style="text-align: center; background-color:#3399ff;" >Quit</button>
+        <button class="btn" id="saveQuiz" style="text-align: center; background-color:#3399ff;" >Save</button>
+        <button class="btn" id="quizBuilderQuit" style="text-align: center; background-color:#3399ff;" >Quit</button>
     `
     }
 
-    _displayInputField() {
+    start() {
+        this.containerElement.innerHTML = this._quizBuilderHTMLBody();
+        this._createInputElements();
+        const button = document.getElementById('quizBuilderQuit');
+        button.onclick = () => this.quit();
+        const startEvent = new Event("QuizBuilder:start");
+        this.containerElement.dispatchEvent(startEvent);
+    }
+
+    _createInputElements() {
+        const newQuestionButton = document.createElement("button");
+        const questionsContainer = document.getElementById("questionsContainer");
+        const quizNameHeader = document.getElementById("quizNameHeader");
+
+        const quizNameInput = document.createElement("input");
+        const questionText = document.createElement("input");
+        const choices = document.createElement("input");
+        const answer = document.createElement("input");
+
+        const questionTextHeader = document.createElement("text");
+        const choicesHeader = document.createElement("text");
+        const answerHeader = document.createElement("text");
+
+        quizNameHeader.appendChild(quizNameInput);
+        questionsContainer.appendChild(questionTextHeader);
+        questionsContainer.appendChild(questionText);
+        questionsContainer.appendChild(choicesHeader);
+        questionsContainer.appendChild(choices);
+        questionsContainer.appendChild(answerHeader);
+        questionsContainer.appendChild(answer);
+        questionsContainer.appendChild(newQuestionButton);
+
+        questionTextHeader.innerHTML = "Type in a question:";
+        choicesHeader.innerHTML = "Type in the choices (comma separated): ";
+        answerHeader.innerHTML = "Type in an answer:";
+
+        quizNameInput.placeholder = "Type in a quiz name..."
+        questionText.placeholder = "Type in a question...";
+        choices.placeholder = "Type in the choices...";
+        answer.placeholder = "Type in an answer...";
+
+        questionTextHeader.className = "inputHeaderStyle"
+        questionTextHeader.className = "inputHeaderStyle";
+        choicesHeader.className = "inputHeaderStyle";
+        answerHeader.className = "inputHeaderStyle";
+        quizNameInput.className = "inputStyles";
+        questionText.className = "inputStyles";
+        choices.className = "inputStyles";
+        answer.className = "newQuestionButton";
+        newQuestionButton.className = "newQuestionButton"
         
     }
 
-    start() {
-        const containerElement = document.getElementById("createQuiz")
-        containerElement.innerHTML = this._quizBuilderHTMLBody();
-        // const button = document.getElementById('quitQuizButton');
-        // button.onclick = () => this.quit();
-        // const startEvent = new Event("SidneyQuiz:start");
-        // this.containerElement.dispatchEvent(startEvent);
+    save() {
+
+    }
+
+    addQuestion() {
+
+    }
+
+    quit() {
+        this.containerElement.textContent = '';
+        const quitEvent = new Event("QuizBuilder:quit");
+        this.containerElement.dispatchEvent(quitEvent);
     }
 }
